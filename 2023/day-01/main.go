33d787ed956094856e3ea036686e4c936f82011f
Jesse Patching <jpatching@usertesting.com>
2023-12-13 15:43:01 -0700

    Solution for 20223 Day 1, Part 2

--- 2023/day-01/main.go
+++ 2023/day-01/main.go
@@ -3,22 +3,24 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/taeram/advent-of-code/internal/utils"
 )
 
 func main() {
 	CalibrationSum := 0
+	CalibrationSumSecond := 0
 	inputs := utils.ReadInputFile()
 	for _, input := range inputs {
-		start, end := FindDigits(input)
-		sum, _ := strconv.Atoi(fmt.Sprintf("%d%d", start, end))
-		CalibrationSum += sum
+		CalibrationSum += FindDigits(input)
+		CalibrationSumSecond += FindDigits(ReplaceNumberWords(input))
 	}
-	utils.Logger("Calibration sum: %d", CalibrationSum)
+	utils.Logger("Part 1 calibration sum: %d", CalibrationSum)
+	utils.Logger("Part 2 calibration sum: %d", CalibrationSumSecond)
 }
 
-func FindDigits(input string) (int, int) {
+func FindDigits(input string) int {
 	var start int
 	var end int
 
@@ -32,5 +34,60 @@ func FindDigits(input string) (int, int) {
 		}
 	}
 
-	return start, end
+	sum, _ := strconv.Atoi(fmt.Sprintf("%d%d", start, end))
+	return sum
+}
+
+func ReplaceNumberWords(input string) string {
+	type Numbers struct {
+		word  string
+		value string
+	}
+
+	numbers := []Numbers{
+		{word: "one", value: "1"},
+		{word: "two", value: "2"},
+		{word: "three", value: "3"},
+		{word: "four", value: "4"},
+		{word: "five", value: "5"},
+		{word: "six", value: "6"},
+		{word: "seven", value: "7"},
+		{word: "eight", value: "8"},
+		{word: "nine", value: "9"},
+	}
+
+	// Iterate over the input string, one character at a time.
+	// Find the first number word in the input string.
+	for i := 0; i < len(input); i++ {
+		// If the current character is a digit, stop processing.
+		if input[i] >= '0' && input[i] <= '9' {
+			break
+		}
+
+		for _, number := range numbers {
+			// If the input starts with a number at the current position, replace it with the numerical value.
+			if strings.HasPrefix(input[i:], number.word) {
+				input = strings.Replace(input, number.word, number.value, 1)
+				break
+			}
+		}
+	}
+
+	// Find the last number word in the input string.
+	for i := len(input) - 1; i >= 0; i-- {
+		// If the current character is a digit, stop processing.
+		if input[i] >= '0' && input[i] <= '9' {
+			break
+		}
+
+		for _, number := range numbers {
+			// If the input starts with a number at the current position, replace it with the numerical value.
+			if strings.HasPrefix(input[i:], number.word) {
+				input = strings.Replace(input, number.word, number.value, 1)
+				break
+			}
+		}
+	}
+
+	return input
 }
--- 2023/day-01/main_test.go
+++ 2023/day-01/main_test.go
@@ -11,19 +11,41 @@ func TestFindDigits(t *testing.T) {
 	a := assert.New(t)
 
 	calibrationValues := []struct {
-		input string
-		start int
-		end   int
+		input  string
+		output int
 	}{
-		{input: "1abc2", start: 1, end: 2},
-		{input: "pqr3stu8vwx", start: 3, end: 8},
-		{input: "a1b2c3d4e5f", start: 1, end: 5},
-		{input: "treb7uchet", start: 7, end: 7},
+		{input: "1abc2", output: 12},
+		{input: "pqr3stu8vwx", output: 38},
+		{input: "a1b2c3d4e5f", output: 15},
+		{input: "treb7uchet", output: 77},
 	}
 
 	for _, value := range calibrationValues {
-		start, end := FindDigits(value.input)
-		a.Equal(value.start, start)
-		a.Equal(value.end, end)
+		a.Equal(value.output, FindDigits(value.input))
+	}
+}
+
+func TestReplaceNumberWords(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+
+	calibrationValues := []struct {
+		input  string
+		output string
+		number int
+	}{
+		{input: "two1nine", output: "219", number: 29},
+		{input: "eightwothree", output: "8wo3", number: 83},
+		{input: "abcone2threexyz", output: "abc123xyz", number: 13},
+		{input: "xtwone3four", output: "x2ne34", number: 24},
+		{input: "4nineeightseven2", output: "4nineeightseven2", number: 42},
+		{input: "zoneight234", output: "z1ight234", number: 14},
+		{input: "7pqrstsixteen", output: "7pqrst6teen", number: 76},
+		{input: "ggdone3nbmsthreefourninefiveoneightpr", output: "ggd13nbms3495on8pr", number: 18},
+	}
+
+	for _, value := range calibrationValues {
+		a.Equal(value.output, ReplaceNumberWords(value.input))
+		a.Equal(value.number, FindDigits(ReplaceNumberWords(value.input)))
 	}
 }
